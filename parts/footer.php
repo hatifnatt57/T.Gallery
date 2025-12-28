cb435b0ee3271eeef0d9ca18bfa1e6f36fd8be3b
Leonid Shapiro <leonidshapiro@yandex.ru>
2025-12-29 00:34:50 +0200

    Updated copyright

--- parts/footer.php
+++ parts/footer.php
@@ -1,6 +1,6 @@
 <?php
 echo('<footer>');
-echo('<p>&copy; All rights reserved. T.Gallery. 2023&mdash;2025</p>');
+echo('<p>&copy; All rights reserved. T.Gallery. 2023&mdash;2026</p>');
 echo('<p>Адрес для комментариев: <a href="mailto:tgallery2023@gmail.com">tgallery2023@gmail.com</a></p>');
 echo('</footer>');
 ?>
--- parts/footeren.php
+++ parts/footeren.php
@@ -1,6 +1,6 @@
 <?php
 echo('<footer>');
-echo('<p>&copy; All rights reserved. T.Gallery. 2023&mdash;2025</p>');
+echo('<p>&copy; All rights reserved. T.Gallery. 2023&mdash;2026</p>');
 echo('<p>Feedback and suggestions: <a href="mailto:tgallery2023@gmail.com">tgallery2023@gmail.com</a></p>');
 echo('</footer>');
 ?>
